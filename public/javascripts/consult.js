97a4a2ef542989d9291fc315f16d7cfe0d4ebde0
simon caceres <kcrs2194@gmail.com>
2020-03-10 14:32:28 -0300

    funcion para resetear en el evento click

--- public/javascripts/consult.js
+++ public/javascripts/consult.js
@@ -4,24 +4,31 @@
 $(document).ready(function(){
     $('#jobsForm').submit(function(event){
     event.preventDefault();
+    $('.inyect').html();
     let consulta = $('#job').val().toLowerCase();
     let city = $('#city').val().toLowerCase();
     var textofinal="";
     
     console.log(city);
-        $.ajax({
+    let request = $.ajax({
           url: `https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=${consulta}&location=${city}`, 
           method: "GET"
-        }).done(function(data) {
+        })
+       
+        request.done(function(data) {
+          $('.inyect').html("");
           for (i = 0; i < data.length; i++){
-            $('.card').append(`<hr/><div class="card-header"><h3> Cargo: ${data[i].title}</h3></di>`)
-             $('.card').append(`<div class="card-header"><h5> Ciudad: ${data[i].location}</h5></div>`)
-             $(`.card`).append(`<div class="card-body"><a href="#" onclick="vermas('mas');" id="mas">...[Leer Descripcion]</a><blockquote mb-0" id="desplegar" style="display:none;"><p> Descripcion: </p> <p >  ${data[i].description}</p></blockquote></div>`)
+            $('.inyect').append(`<hr/><div class = "card"><div class="card-header"><h3> Cargo: ${data[i].title}</h3></di>`)
+             $('.inyect').append(`<div class="card-header"><h5> Ciudad: ${data[i].location}</h5></div>`)
+             $(`.inyect`).append(`<div class="card-body"><a href="#" onclick="vermas('mas');" id="mas">...[Leer Descripcion]</a><blockquote mb-0" id="desplegar" style="display:none;"><p> Descripcion: </p> <p >  ${data[i].description}</p></blockquote></div> </div>`)
             console.log(consulta);
           }
             
+        })
+        request.fail(function(request, statusText)
+        {
+            errorMsg();
         });
-  
       });
   
   });
@@ -37,4 +44,15 @@ $(document).ready(function(){
     }
     }
 
+    const errorMsg = function()
+{
     
+    $(`#respuesta`).text('Ups! Parece que hubo un problema');
+    $(`#respuesta`).text('Por favor intenta nuevamente.');
+    
+}
+
+$('.btn:reset').click(function (ev) {
+  ev.preventDefault();
+  $('.inyect').html("");
+});
